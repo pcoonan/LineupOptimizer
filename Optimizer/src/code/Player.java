bb2353326b32cf012955f0bb9d6dc3c05ceda386
pcoonan <pcoonan@buffalo.edu>
2015-10-05 19:52:45 -0400

    Added member variables to Player. - PC

--- Optimizer/src/code/Player.java
+++ Optimizer/src/code/Player.java
@@ -2,4 +2,17 @@ package code;
 
 public class Player {
 
+	public String name;
+	public String pos;
+	public double ppg;
+	public double sal;
+	public double value;
+	
+	public Player(String iname, String ipos, double ippg, double isal){
+		name = iname;
+		pos = ipos;
+		ppg = ippg;
+		sal = isal;
+		value = ippg/isal;			// Working idea for the algo
+	}
 }
